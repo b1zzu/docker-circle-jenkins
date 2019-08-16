021e0576e24564e95ae9a2a672f901fefeb0e59f
Davide <david3bizzarri@gmail.com>
2019-08-16 16:05:11 +0200

    build all

--- build.py
+++ build.py
@@ -25,11 +25,13 @@ def parser():
     parser.add_argument(
         'template',
         metavar='TEMPLATE',
-        help='the template to build from templates.yml')
+        help='the template to build from templates.yml, use _all_ to build all of them')
     parser.add_argument(
         'variant',
         metavar='VARIANT',
-        help='the variant of the template to build')
+        nargs='?',
+        default='_all_',
+        help='the variant of the template to build, use _all_ to build all variants')
     parser.add_argument(
         '-p',
         '--push',
@@ -57,7 +59,7 @@ def templates():
     return t
 
 
-templates = templates()
+_templates = templates()
 
 
 def delete_image(image):
@@ -78,16 +80,22 @@ def push_images(images):
         logging.info('pushed image %s', image)
 
 
-def get_info(template, variant):
-    if template not in templates:
+def get_variants(template):
+    if template not in _templates:
         logging.error('the template %s does not exists', template)
         exit(1)
 
-    if variant not in templates[template]:
+    return _templates[template]
+
+
+def get_info(template, variant):
+    _variants = get_variants(template)
+
+    if variant not in _variants:
         logging.error('the variant %s %s does not exists', template, variant)
         exit(1)
 
-    return templates[template][variant]
+    return _variants[variant]
 
 
 def build_variant_from(template, variant, info, from_image=''):
@@ -108,7 +116,7 @@ def build_variant_from(template, variant, info, from_image=''):
     return image
 
 
-def build_variant(template, variant, parents=[]):
+def build_variant(template, variant, parents):
     logging.debug('start building variant %s %s', template, variant)
 
     logging.debug(parents)
@@ -154,7 +162,7 @@ version = arguments.version
 
 def build_image(template, variant):
     images = []
-    for tag, image in build_variant(template, variant).items():
+    for tag, image in build_variant(template, variant, []).items():
         tag = '{}{}:{}-{}'.format(namespace, template, version, tag)
         tag_image(image, tag)
         delete_image(image)
@@ -163,10 +171,25 @@ def build_image(template, variant):
     return images
 
 
+images = []
+
 template = arguments.template
 variant = arguments.variant
 
-images = build_image(template, variant)
+if template == '_all_' and variant != '_all_':
+    logging.warning('variant %s is ignored and all variants will be built',
+                    variant)
+    variant = '_all_'
+
+templates = [template] if template != '_all_' else _templates.keys()
+for t in templates:
+    _variants = get_variants(t)
+
+    variants = [variant] if variant != '_all_' else _variants.keys()
+    for v in variants:
+        i = build_image(t, v)
+        images.extend(i)
+
 if arguments.push:
     push_image(images)
 
