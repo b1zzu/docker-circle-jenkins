4054a09f6a5a45dd1b925f1125227a60ec168ebb
Davide <david3bizzarri@gmail.com>
2019-08-16 10:58:02 +0200

    create cibuild script

---
+++ cibuild.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+NAMESPACE="b1zzu"
+
+OPTIONS="-n ${NAMESPACE}"
+
+if [ ! -z "$CIRCLE_TAG" ]; then
+    OPTIONS="$OPTIONS -v $CIRCLE_TAG -p"
+fi
+
+exec ./build.py $OPTIONS $1
